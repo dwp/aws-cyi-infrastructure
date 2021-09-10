bbac699b427059bd6d33d4d76b2dd0d3ce8ebdc6
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-09-10 17:17:06 +0100

    PG flushes

--- steps/create-cyi-database.sh
+++ steps/create-cyi-database.sh
@@ -9,18 +9,6 @@ set -Eeuo pipefail
         log_cyi_message "$${1}" "create-cyi-database.sh" "Running as: ,$USER"
     }
 
-    CORRELATION_ID="$2"
-    S3_BUCKET="$4"
-    S3_PREFIX="$6"
-    EXPORT_DATE="$8"
-
-    log_wrapper_message "Correlation id is '$CORRELATION_ID', s3 bucket is '$S3_BUCKET', s3 prefix is '$S3_PREFIX' and export date is '$EXPORT_DATE', "
-
-    echo "$CORRELATION_ID" >>     /opt/emr/correlation_id.txt
-    echo "$S3_BUCKET" >>          /opt/emr/s3_bucket.txt
-    echo "$S3_PREFIX" >>          /opt/emr/s3_prefix.txt
-    echo "$EXPORT_DATE" >>        /opt/emr/export_date.txt
-
     log_wrapper_message "Creating CYI database"
 
     hive -e "CREATE DATABASE IF NOT EXISTS ${cyi_db} LOCATION '${published_bucket}/${hive_metastore_location}';"
