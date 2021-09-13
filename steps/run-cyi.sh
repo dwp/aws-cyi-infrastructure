6c666ccd2153cd347d782bdca7760bd23e4d372c
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-09-13 17:59:23 +0100

    Modify run-cyi
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- steps/run-cyi.sh
+++ steps/run-cyi.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-
-source /opt/emr/logging.sh
-
-function log_wrapper_message() {
-    log_cyi_message "$${1}" "run-cyi.sh" "Running as: ,$USER"
-}
-
-CORRELATION_ID=$(cat /opt/emr/correlation_id.txt)
-EXPORT_DATE=$(cat /opt/emr/export_date.txt)
+set -Eeuo pipefail
 
 (
+  source /opt/emr/logging.sh
+
+  function log_wrapper_message() {
+      log_cyi_message "$${1}" "run-cyi.sh" "Running as: ,$USER"
+  }
+
+  CORRELATION_ID=$(cat /opt/emr/correlation_id.txt)
+  EXPORT_DATE=$(cat /opt/emr/export_date.txt)
+
   log_wrapper_message "Executing temp table creation and merge for export date $${EXPORT_DATE}"
 
   python3 /var/ci/generate_external_table.py --correlation_id "$CORRELATION_ID" --export_date "$EXPORT_DATE"
