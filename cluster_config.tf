1cb3b5ba8b7104622986fc87b540667360490e5a
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-09-29 15:11:43 +0100

    Fmt
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- cluster_config.tf
+++ cluster_config.tf
@@ -32,9 +32,9 @@ resource "aws_s3_bucket_object" "instances" {
       add_master_sg      = aws_security_group.aws_cyi_infrastructure_common.id
       add_slave_sg       = aws_security_group.aws_cyi_infrastructure_common.id
       subnet_id = (
-      local.use_capacity_reservation[local.environment] == true ?
-      data.terraform_remote_state.internal_compute.outputs.cyi_subnet.subnets[index(data.terraform_remote_state.internal_compute.outputs.cyi_subnet.subnets.*.availability_zone, data.terraform_remote_state.common.outputs.ec2_capacity_reservations.emr_m5_16_x_large_2a.availability_zone)].id :
-      data.terraform_remote_state.internal_compute.outputs.cyi_subnet.subnets[index(data.terraform_remote_state.internal_compute.outputs.cyi_subnet.subnets.*.availability_zone, local.emr_subnet_non_capacity_reserved_environments)].id
+        local.use_capacity_reservation[local.environment] == true ?
+        data.terraform_remote_state.internal_compute.outputs.cyi_subnet.subnets[index(data.terraform_remote_state.internal_compute.outputs.cyi_subnet.subnets.*.availability_zone, data.terraform_remote_state.common.outputs.ec2_capacity_reservations.emr_m5_16_x_large_2a.availability_zone)].id :
+        data.terraform_remote_state.internal_compute.outputs.cyi_subnet.subnets[index(data.terraform_remote_state.internal_compute.outputs.cyi_subnet.subnets.*.availability_zone, local.emr_subnet_non_capacity_reserved_environments)].id
       )
       master_sg                           = aws_security_group.aws_cyi_infrastructure_master.id
       slave_sg                            = aws_security_group.aws_cyi_infrastructure_slave.id
