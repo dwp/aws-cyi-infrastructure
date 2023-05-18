25db3f24af10de0fe6f52a3e4f5d477341f231f5
Sumit Bharadia <36968381+03ce007@users.noreply.github.com>
2023-05-18 14:55:42 +0100

    adds log4j patching as BA (#68)
    * adds log4j patching as BA
    
    * fix: shell lint errors
    
    * fix: shell lint errors
    
    * changes order of patch BA action
    
    * changes order of patch BA action
    
    ---------
    
    Co-authored-by: sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>

--- bootstrap_actions.tf
+++ bootstrap_actions.tf
@@ -89,6 +89,11 @@ resource "aws_s3_bucket_object" "logging_script" {
   }
 }
 
+resource "aws_s3_bucket_object" "patch_log4j_emr_sh" {
+  bucket  = data.terraform_remote_state.common.outputs.config_bucket.id
+  key     = "component/cyi/patch-log4j-emr-6.3.1-v2.sh"
+  content = file("${path.module}/bootstrap_actions/patch-log4j-emr-6.3.1-v2.sh")
+}
 resource "aws_cloudwatch_log_group" "aws_cyi_infrastructure" {
   name              = local.cw_agent_log_group_name
   retention_in_days = 180
